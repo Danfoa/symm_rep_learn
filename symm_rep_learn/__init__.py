f733ac6e2aabf37ee33ee3b7b538e1e58d60b2f3
Daniel Ordonez <daniels.ordonez@gmail.com>
2025-07-23 15:36:31 +0100

    v0.1.2-feat(NCP): enhance NCP model with running statistics and spectral normalization
    - Updated NCP model to include running statistics and momentum parameters.
    - Added spectral normalization to the low-rank approximation operator.
    - Centered embedding functions in the forward pass.
    - Updated loss function to utilize centered embeddings.
    - Modified example usage to reflect changes in NCP initialization.

--- symm_rep_learn/__init__.py
+++ symm_rep_learn/__init__.py
@@ -2,4 +2,4 @@
 symm_rep_learn: Learning Disentangled Representations for Equivariant Inference
 """
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
--- symm_rep_learn/models/ncp.py
+++ symm_rep_learn/models/ncp.py
@@ -4,11 +4,11 @@ from __future__ import annotations
 import logging
 
 import torch
+from symm_learning.nn import DataNorm
+import torch.nn.utils.parametrize as P
+import torch.nn.utils.parametrizations as param
 
-from symm_rep_learn.mysc.statistics import (
-    cov_norm_squared_unbiased_estimation,
-    cross_cov_norm_squared_unbiased_estimation,
-)
+from symm_rep_learn.nn import contrastive_low_rank_loss, orthonormality_regularization
 
 log = logging.getLogger(__name__)
 
@@ -23,42 +23,57 @@ class NCP(torch.nn.Module):
         embedding_dim_y: int,
         gamma=0.1,  # Will be multiplied by the embedding_dim
         gamma_centering=0.01,  # Penalizes probability mass distortion
+        running_stats=False,  # If True, uses running mean and cov for centering embeddings
+        momentum=None,  # None: Cumulative average running stats, float: Exponential moving average wih momentum
     ):
         super(NCP, self).__init__()
         self.gamma = gamma
         self.gamma_centering = gamma_centering
 
-        self.embedding_dim = embedding_dim_x
         self.dim_fx = embedding_dim_x
         self.dim_hy = embedding_dim_y
 
         self.embedding_x = embedding_x
         self.embedding_y = embedding_y
 
-        self._create_op_parameters()
-        # DD
-        # Register buffers for the statistics of the embedding functions
-        self._register_stats_buffers()
+        # Initialize the parameters of the low-rank approximation of the operator
+        self.Dr = torch.nn.Linear(in_features=self.dim_hy, out_features=self.dim_fx, bias=False)
+        # Initialization close to identity. Full rank operator.
+        self.Dr.weight.data = torch.eye(self.dim_fx, self.dim_hy) + 1e-4 * torch.randn(self.dim_fx, self.dim_hy)
+        # Ensure the truncated operator has spectral norm maximum of 1
+        torch.nn.utils.parametrizations.spectral_norm(module=self.Dr, name="weight")
+
+        # Layers that center the embedding functions and keep track of mean and covariance. Variance is penalized in orthonormality
+        self.data_norm_x = DataNorm(
+            embedding_dim_x, momentum=momentum, running_stats=running_stats, compute_cov=True, only_centering=True
+        )
+        self.data_norm_y = DataNorm(
+            embedding_dim_y, momentum=momentum, running_stats=running_stats, compute_cov=True, only_centering=True
+        )
 
     def forward(self, x: torch.Tensor, y: torch.Tensor):
         """Forward pass of the NCP operator.
 
         Computes non-linear transformations of the input random variables x and y, and returns r-dimensional embeddings
         f(x) = [f_1(x), ..., f_r(x)] and h(y) = [h_1(y), ..., h_r(y)] representing the top r-singular functions of
-        the conditional expectation operator such that E_p(y|x)[h_i(y)] = s_i f_i(x) for i=1,...,r.
+        the conditional expectation operator such that E_p(y|x)[h_i(y)] = σ_i f_i(x) for i=1,...,r.
 
         Args:
             x: (torch.Tensor) of shape (..., d) representing the input random variable x.
             y: (torch.Tensor) of shape (..., d) representing the input random variable y.
 
         Returns:
-            fx: (torch.Tensor) of shape (..., r) representing the singular functions of a subspace of L^2(X).
-            hy: (torch.Tensor) of shape (..., r) representing the singular functions of a subspace of L^2(Y).
+            fx_c: (torch.Tensor) of shape (..., r) *centered* basis functions of subspace of L^2(X) .
+            hy_c: (torch.Tensor) of shape (..., r) *centered* basis functions of subspace of L^2(Y) .
         """
         fx = self.embedding_x(x)  # f(x) = [f_1(x), ..., f_r(x)]
         hy = self.embedding_y(y)  # h(y) = [h_1(y), ..., h_r(y)]
 
-        return fx, hy
+        # Center the embedding functions and update the statistics
+        fx_c = self.data_norm_x(fx)  # f_c(x) = f(x) - E_p(x)[f(x)]
+        hy_c = self.data_norm_y(hy)  # h_c(y) = h(y) - E_p(y)[h(y)]
+
+        return fx_c, hy_c
 
     def pointwise_mutual_dependency(self, x: torch.Tensor, y: torch.Tensor):
         """Return the estimated pointwise mutual dependency between the random variables x and y.
@@ -71,254 +86,101 @@ class NCP(torch.nn.Module):
             k_r:  (torch.Tensor) of shape (N,) representing the approximated pointwise mutual dependency between x and
              y defined as PMD = p(x,y)/p(x)p(y) ≈ k_r(x,y).
         """
-        fx, hy = self(x, y)
-        fx_c, hy_c = fx - self.mean_fx, hy - self.mean_hy
+        fx_c, hy_c = self(x, y)
 
         Dr = self.truncated_operator
         k_r = 1 + torch.einsum("...x,xy,...y->...", fx_c, Dr, hy_c)
 
         return k_r
 
-    def pointwise_mutual_information(self, x: torch.Tensor, y: torch.Tensor):
-        """Return the estimated pointwise mutual information between the random variables x and y.
+    @property
+    def truncated_operator(self):
+        """Return the truncated operator"""
+        return self.Dr.weight  # Triggers the spectral normalization.
 
-        Args:
-            x: (torch.Tensor) of shape (N, dx) representing the input random variable x.
-            y: (torch.Tensor) of shape (N, dy) representing the input random variable y.
-
-        Returns:
-            pmi: (torch.Tensor) of shape (N,) representing the approximated  pointwise mutual information between x and
-            y defined as PMI := ln(p(x,y) / p(x)p(y)) ≈ ln(k_r(x,y)).
-        """
-        k_r = self.pointwise_mutual_dependency(x, y)
-        k_r_pos = torch.clamp(k_r, min=1e-6)  # Need to clamp to avoid NaNs.
-        pmi = torch.log(k_r_pos)
-        # Check no NaN  or Inf values
-        assert torch.isfinite(pmi).all(), "NaN or Inf values found in the PMI estimation"
-        return pmi
-
-    def loss(self, fx: torch.Tensor, hy: torch.Tensor):
+    def loss(self, fx_c: torch.Tensor, hy_c: torch.Tensor):
         """TODO.
 
         Args:
-            fx: (torch.Tensor) of shape (..., r) representing the singular functions of a subspace of L^2(X)
-            hy: (torch.Tensor) of shape (..., r) representing the singular functions of a subspace of L^2(Y)
+            fx_c: (torch.Tensor) of shape (..., r) *centered* embedding functions of a subspace of L^2(X)
+            hy_c: (torch.Tensor) of shape (..., r) *centered* embedding functions of a subspace of L^2(Y)
 
         Returns:
             loss: L = -2 ||Cxy||_F^2 + tr(Cxy Cx Cxy^T Cy) - 1
-                + gamma(||Cx - I||_F^2 + ||Cy - I||_F^2 + ||E_p(x) f(x)||_F^2 + ||E_p(y) h(y)||_F^2)
+                + γ(||Cx - I||_F^2 + ||Cy - I||_F^2 + ||E_p(x) f(x)||_F^2 + ||E_p(y) h(y)||_F^2)
             metrics: Scalar valued metrics to monitor during training.
         """
-        assert fx.shape[-1] == hy.shape[-1] == self.embedding_dim, (
-            f"Expected number of singular functions to be {self.embedding_dim}, got {fx.shape[-1]} and {hy.shape[-1]}."
-        )
-
-        # Center basis functions and update mean_fx, mean_hy, Cx, Cy, Cxy
-        fx_c, hy_c = self.update_fns_statistics(fx, hy)
-
+        assert fx_c.shape[-1] == self.dim_fx, f"Expected fx (..., {self.dim_fx}), got {fx_c.shape}"
+        assert hy_c.shape[-1] == self.dim_hy, f"Expected hy (..., {self.dim_hy}), got {hy_c.shape}"
+        dx = self.dim_fx
+        dy = self.dim_hy
         # Orthonormal regularization and centering penalization _________________________________________
+        Cfx, Chy = self.data_norm_x.cov, self.data_norm_y.cov
+        fx_mean, hy_mean = self.data_norm_x.mean, self.data_norm_y.mean
         # orthonormal_reg_fx = ||Cx - I||_F^2 + 2 ||E_p(x) f(x)||_F^2
+        orthonormal_reg_x, metrics_x = orthonormality_regularization(x=fx_c, Cx=Cfx, x_mean=fx_mean, var_name="x")
         # orthonormal_reg_hy = ||Cy - I||_F^2 + 2 ||E_p(y) h(y)||_F^2
-        orthonormal_reg_fx, orthonormal_reg_hy, metrics = self.orthonormality_penalization(fx_c, hy_c)
+        orthonormal_reg_y, metrics_y = orthonormality_regularization(x=hy_c, Cx=Chy, x_mean=hy_mean, var_name="y")
+
+        metrics = metrics_x | metrics_y  # Combine metrics from both regularizations
 
         # Operator truncation error = ||E - E_r||_HS^2 ____________________________________________________
         # E_r = Cxy -> ||E - E_r||_HS - ||E||_HS = -2 ||Cxy||_F^2 + tr(Cxy Cy Cxy^T Cx)
-        truncation_err, loss_metrics = self.unbiased_truncation_error_matrix_form(fx_c, hy_c)
+        Dr = self.truncated_operator
+        clora_err, loss_metrics = contrastive_low_rank_loss(fx_c, hy_c, Dr)
 
         # Lagrange multiplier term for centering constraint of basis functions
-        cent_reg_x = self.mean_fx.pow(2).sum()  #  ||E_x f(x)||_F^2
-        cent_reg_y = self.mean_hy.pow(2).sum()  #  ||E_y h(y)||_F^2
-        cent_reg = cent_reg_x / self.dim_fx + cent_reg_y / self.dim_hy
+        cent_reg_x = fx_mean.pow(2).sum()  #  ||E_x f(x)||_F^2
+        cent_reg_y = hy_mean.pow(2).sum()  #  ||E_y h(y)||_F^2
+        cent_reg = cent_reg_x / dx + cent_reg_y / dy
 
         metrics |= loss_metrics if loss_metrics is not None else {}
         # Total loss ____________________________________________________________________________________
         loss = (
-            truncation_err
-            + self.gamma * (orthonormal_reg_fx / self.dim_fx + orthonormal_reg_hy / self.dim_hy)
-            + self.gamma_centering * cent_reg
+            clora_err + self.gamma * (orthonormal_reg_x / dx + orthonormal_reg_y / dy) + self.gamma_centering * cent_reg
         )
         # Logging metrics _______________________________________________________________________________
         with torch.no_grad():
             metrics |= {
-                "||k(x,y) - k_r(x,y)||": truncation_err.detach().item(),
+                "||k(x,y) - k_r(x,y)||": clora_err.detach().item(),
             }
         return loss, metrics
-
-    def unbiased_truncation_error_matrix_form(self, fx_c, hy_c) -> tuple[torch.Tensor, dict]:
-        """Implementation of ||E - E_r||_HS^2, while assuming E_r is a full matrix.
-
-        Case 1: Orthogonal basis functions give:
-            E_r = Cxy -> ||E - E_r||_HS - ||E||_HS = -2 ||Cxy||_F^2 + tr(Cxy Cy Cxy^T Cx) + 1
-        Case 2: TODO: Trainable E_r matrix
-
-        Args:
-            fx_c: (torch.Tensor) of shape (n_samples, r) representing the centered singular functions of a subspace
-            of L^2(X).
-            hy_c: (torch.Tensor) of shape (n_samples, r) representing the centered singular functions of a subspace
-            of L^2(Y).
-
-        Returns:
-            (torch.Tensor) representing the unbiased truncation error.
-            (dict) Metrics to monitor during training.
-        """
-        metrics = {}
-        n_samples = fx_c.shape[0]
-        Dr = self.truncated_operator  # Dr = Dr.T
-        # k_r(x,y) = 1 + f(x)^T Dr h(y)
-        # truncated_err = -2 * E_p(x,y)[k_r(x,y)] + E_p(x)p(y)[k_r(x,y)^2]
-        pmd_mat = 1 + torch.einsum("nx,xy,my->nm", fx_c, Dr, hy_c)  # (n_samples, n_samples)
-        # E_p(x,y)[k_r(x,y)] = diag(pmd_mat).mean()
-        E_pxy_kr = torch.diag(pmd_mat).mean()
-        pmd_squared = pmd_mat**2
-        # E_p(x)p(y)[k_r(x,y)^2]  # Note we remove the samples from the joint in the diagonal
-        E_px_py_kr = (pmd_squared.sum() - pmd_squared.diag().sum()) / (n_samples * (n_samples - 1))
-        truncation_err = (-2 * E_pxy_kr) + (E_px_py_kr) + 1
-
-        with torch.no_grad():
-            prob_mass_distortion = (1 - pmd_mat.mean()) ** 2  # Always 0 for batch due to centering.
-            metrics |= {
-                "E_p(x)p(y) k_r(x,y)^2": E_px_py_kr.detach() - 1,
-                "E_p(x,y) k_r(x,y)": E_pxy_kr.detach() - 1,
-                "Prob Mass Distortion": prob_mass_distortion,
-            }
-
-        return truncation_err, metrics
-
-    def orthonormality_penalization(self, fx_c, hy_c, permutation=None):
-        """Computes orthonormality and centering regularization penalization for a batch of feature vectors.
-
-        Computes finite sample unbiased empirical estimates of the term:
-        || Vx - I ||_F^2 = || Cx - I ||_F^2 + 2 || E_p(x) f(x) ||^2
-                         = tr(Cx^2) - 2 tr(Cx) + r + 2 || E_p(x) f(x) ||^2
-        Where Vx ∈ R^{r+1 x r+1} is the uncentered covariance matrix assuming the constant function is included as the
-        first fn. Cx ∈ R^{r x r} is the centered covariance matrix of the feature vectors f_c(x) = f(x) - mean(f(x)).
-        That is, [Vx]_ij = E_p(x) f_i(x) f_j(x) and [Cx]_ij = E_p(x) f_c,i(x) f_c,j(x), where f_c = f - E_p(x) f(x).
-
-        The unbiased estimate requires to have two sample sets of marginal distribution p(x), such that
-        D = {x_1, ..., x_n} and D' = {x'_1, ..., x'_n} are independent sampling sets from p(x). To achieve this in
-        practice we shuffle D to obtain D' and to get f(x) and f(x').
-        Then we have that the unbiased estimate is computed by:
-
-        || Vx - I ||_F^2 ≈ E_(x,x')~p(x) [(f_c(x).T f_c(x'))^2] - 2 tr(Cx) + r + 2 ||f_mean||^2
-
-        Args:
-            fx_c: (n_samples, r) Centered feature vectors f_c(x) = [f_c,1(x), ..., f_c,r(x)].
-            hy_c: (n_samples, r) Centered feature vectors h_c(y) = [h_c,1(y), ..., h_c,r(y)].
-            return_inner_prod: (bool) If True, return intermediate inner products.
-            permutation: (torch.Tensor) Permutation tensor to shuffle the samples in the batch.
-
-        Returns:
-            Regularization term as a scalar tensor.
-        """
-        # Compute unbiased empirical estimates ||Cx||_F^2 = E_(x,x')~p(x) [(f_c(x).T f_c(x'))^2]
-        Cx_fro_2 = cov_norm_squared_unbiased_estimation(fx_c, False, permutation=permutation)
-        tr_Cx = torch.trace(self.Cx)  # E[f_c(x)^T f_c(x)] =  tr(Cx)
-        fx_centering_loss = (self.mean_fx**2).sum()  # ||E_p(x) (f(x_i))||^2
-        embedding_dim_x = fx_c.shape[-1]
-
-        Cy_fro_2 = cov_norm_squared_unbiased_estimation(hy_c, False, permutation=permutation)
-        tr_Cy = torch.trace(self.Cy)  # E[h_c(y)^T h_c(y)] = tr(Cy)
-        hy_centering_loss = (self.mean_hy**2).sum()  # ||E_p(y) (h(y_i))||^2
-        embedding_dim_y = hy_c.shape[-1]
-
-        # orthonormality_fx = tr(Cx^2) - 2 tr(Cx) # + 2 || E_p(x) f(x) ||^2 + r_x = |Fx|
-        orthonormality_fx = Cx_fro_2 - 2 * tr_Cx + embedding_dim_x + 2 * fx_centering_loss
-        # orthonormality_hy = tr(Cy^2) - 2 tr(Cy) + 2 || E_p(y) h(y) ||^2 + r_y = |Hy|
-        orthonormality_hy = Cy_fro_2 - 2 * tr_Cy + embedding_dim_y + 2 * hy_centering_loss
-
-        with torch.no_grad():
-            # Divide by the embedding dimension to standardize metrics across experiments.
-            metrics = {
-                "tr(Cx)": (tr_Cx / embedding_dim_x).item(),
-                "||mu_x||": torch.sqrt(fx_centering_loss).item(),
-                "tr(Cy)": (tr_Cy / embedding_dim_y).item(),
-                "||mu_y||": torch.sqrt(hy_centering_loss).item(),
-            }
-
-        return orthonormality_fx, orthonormality_hy, metrics
-
-    def update_fns_statistics(self, fx: torch.Tensor, hy: torch.Tensor):
-        """Update the statistics of the embedding functions.
-
-        Computes the mean and covariance matrices of the embedding functions f(x) and h(y) for the batch of samples.
-
-        Args:
-            fx: (torch.Tensor) of shape (n_samples, r) representing the basis functions of a subspace of L^2(X).
-            hy: (torch.Tensor) of shape (n_samples, r) representing the basis functions of a subspace of L^2(Y).
-
-        Returns:
-            fx_c: (torch.Tensor) centered basis functions
-            hy_c: (torch.Tensor) centered basis functions
-        """
-        n_samples = fx.shape[0]
-
-        eps_x = 1e-6 * torch.eye(self.dim_fx, device=fx.device, dtype=fx.dtype)
-        eps_y = 1e-6 * torch.eye(self.dim_hy, device=hy.device, dtype=hy.dtype)
-        # eps_xy = 1e-6 * torch.eye(self.embedding_dim_x, self.embedding_dim_y, device=fx.device, dtype=fx.dtype)
-
-        self.mean_fx = fx.mean(dim=0, keepdim=True)
-        self.mean_hy = hy.mean(dim=0, keepdim=True)
-
-        # Centering before (centered/un-centered) covariance estimation is key for numerical stability.
-        fx_c, hy_c = fx - self.mean_fx, hy - self.mean_hy
-        self.Cxy = torch.einsum("nr,nc->rc", fx_c, hy_c) / (n_samples - 1)
-        self.Cx = torch.einsum("nr,nc->rc", fx_c, fx_c) / (n_samples - 1) + eps_x
-        self.Cy = torch.einsum("nr,nc->rc", hy_c, hy_c) / (n_samples - 1) + eps_y
-
-        return fx_c, hy_c
-
-    @property
-    def truncated_operator(self):
-        Dr_symm = self.Dr_params @ self.Dr_params.T  # Ensure its symmetric
-        eigval_max = torch.linalg.eigvalsh(Dr_symm)[-1]
-        Dr = Dr_symm / eigval_max
-        return Dr
-
-    def _create_op_parameters(self):
-        embedding_dim = self.embedding_dim
-        D_r = torch.eye(embedding_dim) + 1e-4 * torch.randn(embedding_dim, embedding_dim)
-        self.Dr_params = torch.nn.Parameter(D_r, requires_grad=True)
-
-    def _register_stats_buffers(self):
-        # Matrix containing the cross-covariance matrix form of the operator Cyx: L^2(Y) -> L^2(X)
-        self.register_buffer("Cxy", torch.zeros(self.embedding_dim, self.embedding_dim))
-        # Matrix containing the covariance matrix form of the operator Cx: L^2(X) -> L^2(X)
-        self.register_buffer("Cx", torch.zeros(self.embedding_dim, self.embedding_dim))
-        # Matrix containing the covariance matrix form of the operator Cy: L^2(Y) -> L^2(Y)
-        self.register_buffer("Cy", torch.zeros(self.embedding_dim, self.embedding_dim))
-        # Expectation of the embedding functions
-        self.register_buffer("mean_fx", torch.zeros((1, self.embedding_dim)))
-        self.register_buffer("mean_hy", torch.zeros((1, self.embedding_dim)))
 
 
 if __name__ == "__main__":
     from symm_rep_learn.nn.layers import MLP
 
-    in_dim, out_dim, embedding_dim = 10, 4, 40
+    in_dim, out_dim, embedding_dim_x, embedding_dim_y = 10, 4, 40, 50
 
     fx = MLP(
         input_shape=in_dim,
-        output_shape=embedding_dim,
+        output_shape=embedding_dim_x,
         n_hidden=3,
         layer_size=64,
         activation=torch.nn.GELU,
     )
     hy = MLP(
         input_shape=out_dim,
-        output_shape=embedding_dim,
+        output_shape=embedding_dim_y,
         n_hidden=3,
         layer_size=64,
         activation=torch.nn.GELU,
     )
-    ncp = NCP(fx, hy, embedding_dim_x=embedding_dim, embedding_dim_y=embedding_dim)
+    ncp = NCP(
+        fx, hy, embedding_dim_x=embedding_dim_x, embedding_dim_y=embedding_dim_y, running_stats=True, momentum=0.1
+    )
+    ncp.train()
 
-    x = torch.randn(10, in_dim)
-    y = torch.randn(10, out_dim)
+    for _ in range(10):
+        x = torch.randn(10, in_dim)
+        y = torch.randn(10, out_dim)
 
-    fx, hy = ncp(x, y)
+        f, h = ncp(x, y)
 
-    loss, metrics = ncp.loss(fx, hy)
-    print(loss)
-    with torch.no_grad():
-        pmd = ncp.pointwise_mutual_dependency(x, y)
-    print(pmd)
+        # print(f.shape, h.shape)
+
+        loss, metrics = ncp.loss(f, h)
+        # print(loss.item())
+
+        with torch.no_grad():
+            pmd = ncp.pointwise_mutual_dependency(x, y)
